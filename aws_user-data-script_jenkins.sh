f8afcbf12d254e6afeab56376df8459072b9aac8
osm <osm0822@naver.com>
2024-07-26 01:12:09 +0900

    feat: jenkins서버의 credentials를 제공하는 방식을 변경
    기존의 aws parameter store에서 받아온 정보를 mount해서 넘겨주는 자동화 방식 대신 jenkins의 credentials 관리 기능을 이용해 직접 업로드하도록 변경

--- aws_user-data-script_jenkins.sh
+++ aws_user-data-script_jenkins.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 ###필요한 정보 정의
-#AWS Systems Manager Parameter Store에서 shh,gpg key를 가져옴
-mkdir -p ./credentials
-aws ssm get-parameter --name "/yummyeater/github/config/git-crypt/gpg/private-key" --with-decryption --query "Parameter.Value" --output text > ./credentials/config_gitcrypt_gpg_private_key
-aws ssm get-parameter --name "/yummyeater/github/config/ssh/private-key" --with-decryption --query "Parameter.Value" --output text > ./credentials/config_github_ssh_private_key
-aws ssm get-parameter --name "/yummyeater/dockerhub/user/username" --with-decryption --query "Parameter.Value" --output text > ./credentials/dockerhub_username
-aws ssm get-parameter --name "/yummyeater/dockerhub/user/password" --with-decryption --query "Parameter.Value" --output text > ./credentials/dockerhub_password
 export DOCKER_IMAGE_PATH="ohretry/yummy-jenkins-linux"
 
 chmod +x ./amazonlinux2023_init_docker.sh ./amazonlinux2023_init_swapfile.sh
@@ -23,7 +17,6 @@ source ./amazonlinux2023_init_swapfile.sh
 echo "================ jenkins 서버 실행 ================"
 # host의 docker socket 와 cli 공유.
 docker run \
-  -v ./credentials:/credentials \
   -v jenkins_home:/var/jenkins_home \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v $(which docker):/usr/bin/docker \
